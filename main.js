cb29555ffebffe76851e3523dbf4c70529749106
Arup Debnath <arupdebn@gmail.com>
2021-02-18 22:54:12 +0600

    Sweet Demo

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelector(".first").addEventListener('click', function(){
+    Swal.fire("Our First Alert");
+  });
+  
+  document.querySelector(".second").addEventListener('click', function(){
+    Swal.fire("Our First Alert", "With some body text!");
+  });
+  
+  document.querySelector(".third").addEventListener('click', function(){
+    Swal.fire("Our First Alert", "With some body text and success icon!", "success");
+  });document.querySelector(".first").addEventListener('click', function(){
+    Swal.fire("Our First Alert");
+  });
+  
+  document.querySelector(".second").addEventListener('click', function(){
+    Swal.fire("Our First Alert", "With some body text!");
+  });
+  
+  document.querySelector(".third").addEventListener('click', function(){
+    Swal.fire("Our First Alert", "With some body text and success icon!", "success");
+  });
